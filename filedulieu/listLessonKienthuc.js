08b8d9fda7bd6be61b1c9dddeed75a9218f01522
Dienpham <dienpham187294@gmail.com>
2022-02-27 22:24:03 +0700

    add

--- filedulieu/listLessonKienthuc.js
+++ filedulieu/listLessonKienthuc.js
@@ -17,136 +17,30 @@ module.exports = [
         "fileName": "B1_mergerandquisition/bai1",
         "videoSrc": "https://www.youtube.com/embed/WBHzCDgEtVQ"
     },
-    {
-        "id": "merger-acquisition-2",
-        "name": "Merger & Acquistion (M&A) Deal Structures Explained",
-        "fileName": "B1_mergerandquisition/bai2",
-        "videoSrc": "https://www.youtube.com/embed/ASUM5illI98"
-    },
-    {
-        "id": "merger-acquisition-3",
-        "name": "Mergers and Acquisitions Due Diligence Explained",
-        "fileName": "B1_mergerandquisition/bai3",
-        "videoSrc": "https://www.youtube.com/embed/zVZSS88gIGQ"
-    },
-    {
-        "id": "merger-acquisition-4",
-        "name": "3 Reasons to Consider Seller Financing in the Sale of Your Business",
-        "fileName": "B1_mergerandquisition/bai4",
-        "videoSrc": "https://www.youtube.com/embed/1Gyw5X02ISc"
-    },
-    {
-        "id": "merger-acquisition-5",
-        "name": "M&A Letters of Intent: Are They Really Necessary?",
-        "fileName": "B1_mergerandquisition/bai5",
-        "videoSrc": "https://www.youtube.com/embed/aNioqOmfOe4"
-    },
-    {
-        "id": "merger-acquisition-6",
-        "name": "How to Structure Earnouts in Business Mergers & Acquisitions",
-        "fileName": "B1_mergerandquisition/bai6",
-        "videoSrc": "https://www.youtube.com/embed/Q3DfoFgXLwY"
-    },
-    {
-        "id": "merger-acquisition-7",
-        "name": "EBITDA vs SDE: What's the Difference? | A Business Lawyer Explains",
-        "fileName": "B1_mergerandquisition/bai7",
-        "videoSrc": "https://www.youtube.com/embed/lN1XUxvPKQA"
-    },
-    {
-        "id": "merger-acquisition-8",
-        "name": "Dealing with Debt in a Business Sale or Acquisition",
-        "fileName": "B1_mergerandquisition/bai8",
-        "videoSrc": "https://www.youtube.com/embed/rWu5eV6FhJY"
-    },
-    {
-        "id": "merger-acquisition-9",
-        "name": "Comparing Multiple Offers: Mergers & Acquisitons Explained",
-        "fileName": "B1_mergerandquisition/bai9",
-        "videoSrc": "https://www.youtube.com/embed/DC9-AqEzXmI"
-    },
-    {
-        "id": "merger-acquisition-10",
-        "name": "Indemnification in Mergers & Acquisitions Explained",
-        "fileName": "B1_mergerandquisition/bai10",
-        "videoSrc": "https://www.youtube.com/embed/pnPdfZUGxZU"
-    },
-    {
-        "id": "merger-acquisition-11",
-        "name": "Understanding Private Equity Buyers in Mergers and Acquisitions",
-        "fileName": "B1_mergerandquisition/bai11",
-        "videoSrc": "https://www.youtube.com/embed/xXUU0MLG0Ic"
-    },
-    {
-        "id": "merger-acquisition-12",
-        "name": "Understanding Strategic Buyers in Mergers and Acquisitions",
-        "fileName": "B1_mergerandquisition/bai12",
-        "videoSrc": "https://www.youtube.com/embed/tumylUHE_-0"
-    },
-    {
-        "id": "merger-acquisition-13",
-        "name": "Understanding Individual Buyers in Mergers and Acquisitions",
-        "fileName": "B1_mergerandquisition/bai13",
-        "videoSrc": "https://www.youtube.com/embed/Di6jhXsR1bs"
-    },
-    {
-        "id": "merger-acquisition-14",
-        "name": "Mergers and Acquisitions: A Comprehensive Overview of the M&A Process",
-        "fileName": "B1_mergerandquisition/bai14",
-        "videoSrc": "https://www.youtube.com/embed/LpgBGO0aO-Y"
-    },
-    {
-        "id": "merger-acquisition-15",
-        "name": "Representations and Warranties in Mergers and Acquisitions (M&A)",
-        "fileName": "B1_mergerandquisition/bai15",
-        "videoSrc": "https://www.youtube.com/embed/cnvG-qb6w9Q"
-    },
-    {
-        "id": "merger-acquisition-16",
-        "name": "The One and Two-Step Acquisition Close | Mergers and Acquisitions (M&A)",
-        "fileName": "B1_mergerandquisition/bai16",
-        "videoSrc": "https://www.youtube.com/embed/bf-aVQQaP0U"
-    },
-    {
-        "id": "merger-acquisition-17",
-        "name": "How to Sell Your Business: Auctions vs Direct Negotiation Deals | Mergers and Acquisitions Explained",
-        "fileName": "B1_mergerandquisition/bai17",
-        "videoSrc": "https://www.youtube.com/embed/xoNq0Bb5qgg"
-    },
-    {
-        "id": "merger-acquisition-18",
-        "name": "6 Things to Do Before Selling a Business | Mergers and Acquisitions (M&A)",
-        "fileName": "B1_mergerandquisition/bai18",
-        "videoSrc": "https://www.youtube.com/embed/ArEG-bDHpw4"
-    },
-    {
-        "id": "merger-acquisition-19",
-        "name": "Understanding a Roll-Up M&A Strategy",
-        "fileName": "B1_mergerandquisition/bai19",
-        "videoSrc": "https://www.youtube.com/embed/T31Peld1LEg"
-    },
-    {
-        "id": "merger-acquisition-20",
-        "name": "100% Seller Financing (No Money Down) Businesses",
-        "fileName": "B1_mergerandquisition/bai20",
-        "videoSrc": "https://www.youtube.com/embed/2pnuNJLK-k4"
-    },
-    {
-        "id": "merger-acquisition-21",
-        "name": "8 Ways to Avoid Merger & Acquisitions (M&A) Lawsuits",
-        "fileName": "B1_mergerandquisition/bai21",
-        "videoSrc": "https://www.youtube.com/embed/2Z51XwsUTGw"
-    },
-    {
-        "id": "merger-acquisition-22",
-        "name": "M&A Deal Structures: Working Capital Adjustments vs. Locked Box Closing Approach",
-        "fileName": "B1_mergerandquisition/bai22",
-        "videoSrc": "https://www.youtube.com/embed/ydfu0lpiDjc"
-    },
-    {
-        "id": "merger-acquisition-23",
-        "name": "What is M&A (Mergers & Acquisitions) (in 60 seconds)",
-        "fileName": "B1_mergerandquisition/bai23",
-        "videoSrc": "https://www.youtube.com/embed/VKjPLqG4em4"
-    }
+    { "id": "merger-acquisition-2", "name": "Merger & Acquistion (M&A) Deal Structures Explained", "fileName": "B1_mergerandquisition/bai2", "videoSrc": "https://www.youtube.com/embed/ASUM5illI98" },
+    { "id": "merger-acquisition-3", "name": "Mergers and Acquisitions Due Diligence Explained", "fileName": "B1_mergerandquisition/bai3", "videoSrc": "https://www.youtube.com/embed/zVZSS88gIGQ" },
+    { "id": "merger-acquisition-4", "name": "3 Reasons to Consider Seller Financing in the Sale of Your Business", "fileName": "B1_mergerandquisition/bai4", "videoSrc": "https://www.youtube.com/embed/1Gyw5X02ISc" },
+    { "id": "merger-acquisition-5", "name": "M&A Letters of Intent: Are They Really Necessary?", "fileName": "B1_mergerandquisition/bai5", "videoSrc": "https://www.youtube.com/embed/aNioqOmfOe4" },
+    { "id": "merger-acquisition-6", "name": "How to Structure Earnouts in Business Mergers & Acquisitions", "fileName": "B1_mergerandquisition/bai6", "videoSrc": "https://www.youtube.com/embed/Q3DfoFgXLwY" },
+    { "id": "merger-acquisition-7", "name": "EBITDA vs SDE: What's the Difference? | A Business Lawyer Explains", "fileName": "B1_mergerandquisition/bai7", "videoSrc": "https://www.youtube.com/embed/lN1XUxvPKQA" },
+    { "id": "merger-acquisition-8", "name": "Dealing with Debt in a Business Sale or Acquisition", "fileName": "B1_mergerandquisition/bai8", "videoSrc": "https://www.youtube.com/embed/rWu5eV6FhJY" },
+    { "id": "merger-acquisition-9", "name": "Comparing Multiple Offers: Mergers & Acquisitons Explained", "fileName": "B1_mergerandquisition/bai9", "videoSrc": "https://www.youtube.com/embed/DC9-AqEzXmI" },
+    { "id": "merger-acquisition-10", "name": "Indemnification in Mergers & Acquisitions Explained", "fileName": "B1_mergerandquisition/bai10", "videoSrc": "https://www.youtube.com/embed/pnPdfZUGxZU" },
+    { "id": "merger-acquisition-11", "name": "Understanding Private Equity Buyers in Mergers and Acquisitions", "fileName": "B1_mergerandquisition/bai11", "videoSrc": "https://www.youtube.com/embed/xXUU0MLG0Ic" },
+    { "id": "merger-acquisition-12", "name": "Understanding Strategic Buyers in Mergers and Acquisitions", "fileName": "B1_mergerandquisition/bai12", "videoSrc": "https://www.youtube.com/embed/tumylUHE_-0" },
+    { "id": "merger-acquisition-13", "name": "Understanding Individual Buyers in Mergers and Acquisitions", "fileName": "B1_mergerandquisition/bai13", "videoSrc": "https://www.youtube.com/embed/Di6jhXsR1bs" },
+    { "id": "merger-acquisition-14", "name": "Mergers and Acquisitions: A Comprehensive Overview of the M&A Process", "fileName": "B1_mergerandquisition/bai14", "videoSrc": "https://www.youtube.com/embed/LpgBGO0aO-Y" },
+    { "id": "merger-acquisition-15", "name": "Representations and Warranties in Mergers and Acquisitions (M&A)", "fileName": "B1_mergerandquisition/bai15", "videoSrc": "https://www.youtube.com/embed/cnvG-qb6w9Q" },
+    { "id": "merger-acquisition-16", "name": "The One and Two-Step Acquisition Close | Mergers and Acquisitions (M&A)", "fileName": "B1_mergerandquisition/bai16", "videoSrc": "https://www.youtube.com/embed/bf-aVQQaP0U" },
+    { "id": "merger-acquisition-17", "name": "How to Sell Your Business: Auctions vs Direct Negotiation Deals | Mergers and Acquisitions Explained", "fileName": "B1_mergerandquisition/bai17", "videoSrc": "https://www.youtube.com/embed/xoNq0Bb5qgg" },
+    { "id": "merger-acquisition-18", "name": "6 Things to Do Before Selling a Business | Mergers and Acquisitions (M&A)", "fileName": "B1_mergerandquisition/bai18", "videoSrc": "https://www.youtube.com/embed/ArEG-bDHpw4" },
+    { "id": "merger-acquisition-19", "name": "Understanding a Roll-Up M&A Strategy", "fileName": "B1_mergerandquisition/bai19", "videoSrc": "https://www.youtube.com/embed/T31Peld1LEg" },
+    { "id": "merger-acquisition-20", "name": "100% Seller Financing (No Money Down) Businesses", "fileName": "B1_mergerandquisition/bai20", "videoSrc": "https://www.youtube.com/embed/2pnuNJLK-k4" },
+    { "id": "merger-acquisition-21", "name": "8 Ways to Avoid Merger & Acquisitions (M&A) Lawsuits", "fileName": "B1_mergerandquisition/bai21", "videoSrc": "https://www.youtube.com/embed/2Z51XwsUTGw" },
+    { "id": "merger-acquisition-22", "name": "M&A Deal Structures: Working Capital Adjustments vs. Locked Box Closing Approach", "fileName": "B1_mergerandquisition/bai22", "videoSrc": "https://www.youtube.com/embed/ydfu0lpiDjc" },
+    { "id": "merger-acquisition-23", "name": "What is M&A (Mergers & Acquisitions) (in 60 seconds)", "fileName": "B1_mergerandquisition/bai23", "videoSrc": "https://www.youtube.com/embed/VKjPLqG4em4" },
+    { "id": "merger-acquisition-24", "name": "The 4 Main Categories of Merger & Acquisition (M&A) Deals", "fileName": "B1_mergerandquisition/bai24", "videoSrc": "https://www.youtube.com/embed/1290srbuHcM"},
+    { "id": "merger-acquisition-25", "name": "PART 1: Why Do Businesses Buy Other Businesses? (M&A Motivations)", "fileName": "B1_mergerandquisition/bai25", "videoSrc": "https://www.youtube.com/embed/Xd1FWmkjXFQ" },
+    { "id": "merger-acquisition-26", "name": "PART 2: Why Do Businesses Buy Other Businesses? (M&A Motivations)", "fileName": "B1_mergerandquisition/bai26", "videoSrc": "https://www.youtube.com/embed/9NZMrMWcAag" },
+    { "id": "merger-acquisition-27", "name": "PART 3: Why Do Businesses Buy Other Businesses? (M&A Motivations)", "fileName": "B1_mergerandquisition/bai27", "videoSrc": "https://www.youtube.com/embed/WKH8qkHszU4" },
 ]
--- router.js
+++ router.js
@@ -8,11 +8,11 @@ const jsonParser = bodyParser.json()
 router.get("/", (req, res) => {
   res.send("Server is up and running. Success").status(200);
 });
-router.get("/menuThuchanhcoban", (req, res) => {  
+router.get("/menuThuchanhcoban", (req, res) => {
   const data = require("./filedulieu/listLesson")
   res.send({ "data": data }).status(200);
 });
-router.get("/menuThuchanhkienthuc", (req, res) => {  
+router.get("/menuThuchanhkienthuc", (req, res) => {
   const data = require("./filedulieu/listLessonKienthuc")
   res.send({ "data": data }).status(200);
 });
@@ -24,6 +24,19 @@ router.post("/loadDataThuchanhkienthuc", jsonParser, (req, res) => {
   const data = require("./filedulieu/" + req.body.id)
   res.send({ "data": data }).status(200);
 });
+router.post("/loadDataVideoSrc", jsonParser, (req, res) => {
+  const data = require("./filedulieu/listOfVideos")
+  let output = ""
+  data.forEach(e => {
+    if (e.id === req.body.id) {
+      output = e.videoSrc
+    }
+  })
+
+  res.send({ "data": output }).status(200);
+});
+
+
 router.get("/loadDataThuchanhnangcao", jsonParser, (req, res) => {
   const data = require("./filedulieu/A1_NhaHang_01/bai1")
   res.send({ "data": data }).status(200);
