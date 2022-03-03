2153d7dc533241f937bc162b597d3f96928e0960
Dienpham <dienpham187294@gmail.com>
2022-03-03 10:18:57 +0700

    add

--- filedulieu/A1_NhaHang_01/bai2_4.js
+++ filedulieu/A1_NhaHang_01/bai2_4.js
@@ -42,10 +42,14 @@ let coerdataoflession = [].concat(
 )
 
 let DataTable = [
-    InA2_Togo.tool.m01.concat(InA2_EatHere.tool.m01),
-    InA2_Togo.tool.m02.concat(InA2_Togo.tool.m03),
-    InA2_Menu.tool.m01,
-    InA2_BabyChair.tool.m01.concat(InA2_Toilet.tool.m01)
+    InA2_Togo.tool.m01.concat(
+        InA2_EatHere.tool.m01,
+        InA2_Togo.tool.m02,
+        InA2_Togo.tool.m03,
+        InA2_Menu.tool.m01,
+        InA2_BabyChair.tool.m01,
+        InA2_Toilet.tool.m01
+    )
 ]
 
 const Bai24
