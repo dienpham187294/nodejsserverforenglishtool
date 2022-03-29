d403595d5fa98bad9b3c64654cabbe3aa8e5768d
Dienpham <dienpham187294@gmail.com>
2022-03-29 21:38:42 +0700

    add

--- filedulieu/A1_OfficeConversation/DulieuInside/A/Z_deparments.js
+++ filedulieu/A1_OfficeConversation/DulieuInside/A/Z_deparments.js
@@ -1,102 +1,102 @@
 module.exports = [
     {
         "nameDepart": "The main lobby!",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/DfCq5rF4/The-Main-Lobby-2.jpg",
         "id": "dep0"
     },
     {
         "nameDepart": "Marketing and Proposals Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/529GFqx7/image.png",
         "id": "dep1"
     },
     {
-        "nameDepart": "sales Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "nameDepart": "Sales Department",
+        "img": "https://i.postimg.cc/QtLZ59YF/vai-tro-to-lon-cua-sale-trong-moi-cong-ty-doanh-nghiep.jpg",
         "id": "dep2"
     },
     {
         "nameDepart": "Project Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/ZR1FFN1y/Project-management-2-Shutterstock-706715482.jpg",
         "id": "dep3"
     },
     {
         "nameDepart": "Designing Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/ZR1FFN1y/Project-management-2-Shutterstock-706715482.jpg",
         "id": "dep4"
     },
     {
         "nameDepart": "Production Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/QCwP7Vq6/ABC.jpg",
         "id": "dep5"
     },
     {
         "nameDepart": "Maintenance Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/xdj2nJpH/lzw1592381982.jpg",
         "id": "dep6"
     },
     {
         "nameDepart": "Store Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/1tgb3pGg/Apple-Store-1.jpg",
         "id": "dep7"
     },
     {
         "nameDepart": "Procurement Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/TYcNGkz7/Procurement-1.jpg",
         "id": "dep8"
     },
     {
         "nameDepart": "Quality Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/3xR1h3CN/104915.jpg",
         "id": "dep9"
     },
     {
         "nameDepart": "Inspection department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/QxP9BsZH/A001.png",
         "id": "dep10"
     },
     {
         "nameDepart": "Packaging Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/5yyy0KPB/packaging.jpg",
         "id": "dep11"
     },
     {
         "nameDepart": "Finance Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/1RbVXsQ4/Finance-nghia-la-gi-1.jpg",
         "id": "dep12"
     },
     {
         "nameDepart": "Dispatch Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/G2GwjFHW/Corner-Console-New.jpg",
         "id": "dep13"
     },
     {
         "nameDepart": "Account Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/4yW8vyQC/sd.jpg",
         "id": "dep14"
     },
     {
         "nameDepart": "Research and Development Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/HkXwdj8p/RD1.jpg",
         "id": "dep15"
     },
     {
         "nameDepart": "Information Technology Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/L5Pt5sQB/infotech.jpg",
         "id": "dep16"
     },
     {
         "nameDepart": "Human Resource Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/1zzD6BYz/human-resources-la-gi-2.jpg",
         "id": "dep17"
     },
     {
         "nameDepart": "Security Department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/zfRX9Ryj/cybersecuritylock-1258.jpg",
         "id": "dep18"
     },
     {
         "nameDepart": "Administration department",
-        "img": "https://i.postimg.cc/85Hpwr9T/139-1390590-our-departments-departments-icon-png-clipart.jpg",
+        "img": "https://i.postimg.cc/dVgJmt5T/t-i-xu-ng.jpg",
         "id": "dep19"
     }
 ]
