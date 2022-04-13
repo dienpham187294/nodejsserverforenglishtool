c26db5949b110c2bd62de9a922b2ba5ee9f4c522
Dienpham <dienpham187294@gmail.com>
2022-04-13 19:03:27 +0700

    add

--- filedulieu/PronunCourses/T_TEMPLATE_01/AmainTem.js
+++ filedulieu/PronunCourses/T_TEMPLATE_01/AmainTem.js
@@ -116,7 +116,7 @@ function getOutputT(DATAUSE) {
                 FnToArrobj(input_01_Body, input_02_Body),
                 End,
                 SpeakFirst,
-                ["Hi"]
+                [ee.password]
             )
         )
     })
