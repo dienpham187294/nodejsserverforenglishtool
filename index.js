446fbb0d4f6f83037dfa232e07f16001112917cb
Dienpham <dienpham187294@gmail.com>
2022-04-18 18:24:57 +0700

    add

--- index.js
+++ index.js
@@ -3,35 +3,35 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const fs = require('file-system');
+// const io = require('socket.io')(server);
+// const fs = require('file-system');
 const router = require("./router")
 const { mdAPISocket } = require('./api/mdApiSocket');
-const MD = new mdAPISocket(io)
+// const MD = new mdAPISocket(io)
 
-const ARR = require("./database/words3000");
+// const ARR = require("./database/words3000");
 const { json } = require('body-parser');
 // const i_DATA_ScoreStatistical = require("./api/Data_SCoreStatistical")
 app.use(cors());
 app.use(router);
-let i = 0
+// let i = 0
 
-io.on('connection', client => {
-    i++
-    console.log("connect_01", client.id, i)
-    io.to(client.id).emit("connect_01", client.id + "_" + i)
-    client.on('disconnect', () => {
-        console.log("disconnected", client.id)
-    });
+// io.on('connection', client => {
+//     i++
+//     console.log("connect_01", client.id, i)
+//     io.to(client.id).emit("connect_01", client.id + "_" + i)
+//     client.on('disconnect', () => {
+//         console.log("disconnected", client.id)
+//     });
 
-    client.on('emit_RES_Client_FirstTime', (data) => {
-        MD.MD_ScoreStatistical_FirstTime(data.id)
-    });
-    client.on('emit_RES_Client', (data) => {
-        MD.MD_W_ScoreStatistical(data)
-        io.emit("emit_RES_Server", data)
-    });
-});
+//     client.on('emit_RES_Client_FirstTime', (data) => {
+//         MD.MD_ScoreStatistical_FirstTime(data.id)
+//     });
+//     client.on('emit_RES_Client', (data) => {
+//         MD.MD_W_ScoreStatistical(data)
+//         io.emit("emit_RES_Server", data)
+//     });
+// });
 server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
