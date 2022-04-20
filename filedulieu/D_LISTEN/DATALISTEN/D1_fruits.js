5d22d81ff1f44ba63e8bf4695308d8fea61653d7
Dienpham <dienpham187294@gmail.com>
2022-04-20 07:03:28 +0700

    add

--- filedulieu/D_LISTEN/DATALISTEN/D1_fruits.js
+++ filedulieu/D_LISTEN/DATALISTEN/D1_fruits.js
@@ -1,122 +1,122 @@
 module.exports = [
     {
-        "Read": "Apple",
+        "Read": "I work at Apple company",
         "Answer": "Apple"
     },
     {
-        "Read": "Gooseberry",
+        "Read": "I work at Gooseberry company",
         "Answer": "Gooseberry"
     },
     {
-        "Read": "Banana",
+        "Read": "I work at Banana company",
         "Answer": "Banana"
     },
     {
-        "Read": "Cherry",
+        "Read": "I work at Cherry company",
         "Answer": "Cherry"
     },
     {
-        "Read": "Orange",
+        "Read": "I work at Orange company",
         "Answer": "Orange"
     },
     {
-        "Read": "Apricot",
+        "Read": "I work at Apricot company",
         "Answer": "Apricot"
     },
     {
-        "Read": "Mango",
+        "Read": "I work at Mango company",
         "Answer": "Mango"
     },
     {
-        "Read": "Jackfruit",
+        "Read": "I work at Jackfruit company",
         "Answer": "Jackfruit"
     },
     {
-        "Read": "Grapes",
+        "Read": "I work at Grapes company",
         "Answer": "Grapes"
     },
     {
-        "Read": "Kiwifruit",
+        "Read": "I work at Kiwifruit company",
         "Answer": "Kiwifruit"
     },
     {
-        "Read": "Pineapple",
+        "Read": "I work at Pineapple company",
         "Answer": "Pineapple"
     },
     {
-        "Read": "Lime",
+        "Read": "I work at Lime company",
         "Answer": "Lime"
     },
     {
-        "Read": "Pomegranate",
+        "Read": "I work at Pomegranate company",
         "Answer": "Pomegranate"
     },
     {
-        "Read": "Peach",
+        "Read": "I work at Peach company",
         "Answer": "Peach"
     },
     {
-        "Read": "Avocado",
+        "Read": "I work at Avocado company",
         "Answer": "Avocado"
     },
     {
-        "Read": "Pear",
+        "Read": "I work at Pear company",
         "Answer": "Pear"
     },
     {
-        "Read": "Coconut",
+        "Read": "I work at Coconut company",
         "Answer": "Coconut"
     },
     {
-        "Read": "Tamarind",
+        "Read": "I work at Tamarind company",
         "Answer": "Tamarind"
     },
     {
-        "Read": "Papaya",
+        "Read": "I work at Papaya company",
         "Answer": "Papaya"
     },
     {
-        "Read": "Custard-Apple",
+        "Read": "I work at Custard-Apple company",
         "Answer": "Custard-Apple"
     },
     {
-        "Read": "Watermelon",
+        "Read": "I work at Watermelon company",
         "Answer": "Watermelon"
     },
     {
-        "Read": "Fig",
+        "Read": "I work at Fig company",
         "Answer": "Fig"
     },
     {
-        "Read": "Dragonfruit",
+        "Read": "I work at Dragonfruit company",
         "Answer": "Dragonfruit"
     },
     {
-        "Read": "Guava",
+        "Read": "I work at Guava company",
         "Answer": "Guava"
     },
     {
-        "Read": "Strawberry",
+        "Read": "I work at Strawberry company",
         "Answer": "Strawberry"
     },
     {
-        "Read": "MuskMelon",
+        "Read": "I work at MuskMelon company",
         "Answer": "MuskMelon"
     },
     {
-        "Read": "Blueberry",
+        "Read": "I work at Blueberry company",
         "Answer": "Blueberry"
     },
     {
-        "Read": "Olives",
+        "Read": "I work at Olives company",
         "Answer": "Olives"
     },
     {
-        "Read": "Blackberry",
+        "Read": "I work at Blackberry company",
         "Answer": "Blackberry"
     },
     {
-        "Read": "Java Plum",
+        "Read": "I work at Java Plum company",
         "Answer": "Java Plum"
     }
 ]
--- filedulieu/D_LISTEN/DATALISTEN/D1_jobs.js
+++ filedulieu/D_LISTEN/DATALISTEN/D1_jobs.js
@@ -1,123 +1,122 @@
 module.exports = [
     {
-        "Read": "an operator",
+        "Read": "I work as an operator",
         "Answer": "an operator"
     },
     {
-        "Read": "a secretary",
-        "Read": "a secretary",
+        "Read": "I work as a secretary",
         "Answer": "a secretary"
     },
     {
-        "Read": "a business manager",
+        "Read": "I work as a business manager",
         "Answer": "a business manager"
     },
     {
-        "Read": "a typist",
+        "Read": "I work as a typist",
         "Answer": "a typist"
     },
     {
-        "Read": "a quality control coordinator",
+        "Read": "I work as a quality control coordinator",
         "Answer": "a quality control coordinator"
     },
     {
-        "Read": "an accountant",
+        "Read": "I work as an accountant",
         "Answer": "an accountant"
     },
     {
-        "Read": "a chief executive officer",
+        "Read": "I work as a chief executive officer",
         "Answer": "a chief executive officer"
     },
     {
-        "Read": "a receptionist",
+        "Read": "I work as a receptionist",
         "Answer": "a receptionist"
     },
     {
-        "Read": "a business analyst",
+        "Read": "I work as a business analyst",
         "Answer": "a business analyst"
     },
     {
-        "Read": "an office manager",
+        "Read": "I work as an office manager",
         "Answer": "an office manager"
     },
     {
-        "Read": "a risk manager",
+        "Read": "I work as a risk manager",
         "Answer": "a risk manager"
     },
     {
-        "Read": "an auditing clerk",
+        "Read": "I work as an auditing clerk",
         "Answer": "an auditing clerk"
     },
     {
-        "Read": "an administrative specialist",
+        "Read": "I work as an administrative specialist",
         "Answer": "an administrative specialist"
     },
     {
-        "Read": "a branch manager",
+        "Read": "I work as a branch manager",
         "Answer": "a branch manager"
     },
     {
-        "Read": "a program administrator",
+        "Read": "I work as a program administrator",
         "Answer": "a program administrator"
     },
     {
-        "Read": "a human resources",
+        "Read": "I work as a human resources",
         "Answer": "a human resources"
     },
     {
-        "Read": "a program manager",
+        "Read": "I work as a program manager",
         "Answer": "a program manager"
     },
     {
-        "Read": "an office assistant",
+        "Read": "I work as an office assistant",
         "Answer": "an office assistant"
     },
     {
-        "Read": "an administrative analyst",
+        "Read": "I work as an administrative analyst",
         "Answer": "an administrative analyst"
     },
     {
-        "Read": "an office clerk",
+        "Read": "I work as an office clerk",
         "Answer": "an office clerk"
     },
     {
-        "Read": "a data entry",
+        "Read": "I work as a data entry",
         "Answer": "a data entry"
     },
     {
-        "Read": "a file clerk",
+        "Read": "I work as a file clerk",
         "Answer": "a file clerk"
     },
     {
-        "Read": "a computer scientist",
+        "Read": "I work as a computer scientist",
         "Answer": "a computer scientist"
     },
     {
-        "Read": "an account executive",
+        "Read": "I work as an account executive",
         "Answer": "an account executive"
     },
     {
-        "Read": "a web developer",
+        "Read": "I work as a web developer",
         "Answer": "a web developer"
     },
     {
-        "Read": "an account collector",
+        "Read": "I work as an account collector",
         "Answer": "an account collector"
     },
     {
-        "Read": "a UX designer",
+        "Read": "I work as a UX designer",
         "Answer": "a UX designer"
     },
     {
-        "Read": "an executive assistant",
+        "Read": "I work as an executive assistant",
         "Answer": "an executive assistant"
     },
     {
-        "Read": "a web designer",
+        "Read": "I work as a web designer",
         "Answer": "a web designer"
     },
     {
-        "Read": "an administrative manager",
+        "Read": "I work as an administrative manager",
         "Answer": "an administrative manager"
     }
 ]
